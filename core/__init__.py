a3adc3d872192bd438187389bf679a35a2350395
lightless <lightless@foxmail.com>
2017-02-16 21:32:11 +0800

    添加工程骨架

---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# coding: utf-8
+# Created by PyCharm
+# Project: baize
+# Filename: __init__.py
+# Time: 2017/2/16
+
+
+__author__ = 'lightless'
+__email__ = 'root@lightless.me'
+
